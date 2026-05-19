c77071833f288bee46180939d361cb9846354a5a
joshuaEA12 <joshuabenjasolanojimenes@gmail.com>
2026-05-19 15:59:37 -0600

    Define atributos y metodos del modelo Service

--- src/main/java/model/Service.java
+++ src/main/java/model/Service.java
@@ -1,4 +1,87 @@
 package model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Service {
+    @Id
+    private Long id;
+    private String title;
+    private String description;
+    private Double price;
+    private String category;
+    private User freelancer;
+
+    public Service() {
+    }
+
+    public Service(Long id, String title, String description, Double price, String category, User freelancer) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.freelancer = freelancer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public User getFreelancer() {
+        return freelancer;
+    }
+
+    public void setFreelancer(User freelancer) {
+        this.freelancer = freelancer;
+    }
+
+    @Override
+    public String toString() {
+        return "Service{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                ", freelancer=" + freelancer +
+                '}';
+    }
 }
