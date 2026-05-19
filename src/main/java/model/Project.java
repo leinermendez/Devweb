900fda8f69bcbf3fe12a3732eef07bd8bafd5aca
joshuaEA12 <joshuabenjasolanojimenes@gmail.com>
2026-05-19 15:41:11 -0600

    Define atributos y metodos del modelo projet

--- src/main/java/model/Project.java
+++ src/main/java/model/Project.java
@@ -1,4 +1,100 @@
 package model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Project {
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String category;
+    private String imageUrl;
+    private LocalDateTime date;
+    private User freelacer;
+
+    public Project() {
+    }
+
+    public Project(String id, String title, String description, String category, String imageUrl, LocalDateTime date, User freelacer) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.imageUrl = imageUrl;
+        this.date = date;
+        this.freelacer = freelacer;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public User getFreelacer() {
+        return freelacer;
+    }
+
+    public void setFreelacer(User freelacer) {
+        this.freelacer = freelacer;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", category='" + category + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", date=" + date +
+                ", freelacer=" + freelacer +
+                '}';
+    }
 }
