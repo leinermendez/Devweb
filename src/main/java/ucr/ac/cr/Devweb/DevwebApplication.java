77773b053310496e2675d18e724c4acb8594df34
Tocinit0 <leiner611@gmail.com>
2026-05-18 18:45:26 -0600

    Creación base del proyecto con los modelos

---
+++ src/main/java/ucr/ac/cr/Devweb/DevwebApplication.java
@@ -0,0 +1,13 @@
+package ucr.ac.cr.Devweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ucr/ac/cr/Devweb/DevwebApplicationTests.java
@@ -0,0 +1,13 @@
+package ucr.ac.cr.Devweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
