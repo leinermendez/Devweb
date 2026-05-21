27f51eb3033369ab69097598c3eb991545bbdcbf
Tocinit0 <leiner611@gmail.com>
2026-05-21 00:07:48 -0600

    "Modulo USER funcional probado en POSTMAN"

--- src/main/java/ucr/ac/cr/Devweb/repository/ReviewRepository.java
+++ src/main/java/ucr/ac/cr/Devweb/repository/ReviewRepository.java
@@ -7,3 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 }
+
--- src/main/java/ucr/ac/cr/Devweb/repository/ServiceRepository.java
+++ src/main/java/ucr/ac/cr/Devweb/repository/ServiceRepository.java
@@ -7,3 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ServiceRepository extends JpaRepository<Service, Integer> {
 }
+
