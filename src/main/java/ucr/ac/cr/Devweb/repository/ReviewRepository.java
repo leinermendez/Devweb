c51bc2fa292a5de963d63efadee8a6d174e596ac
joshuaEA12 <joshuabenjasolanojimenes@gmail.com>
2026-05-21 00:21:14 -0600

    Creacion del metodo para filtrar reviews de un freelancer

--- src/main/java/ucr/ac/cr/Devweb/repository/ReviewRepository.java
+++ src/main/java/ucr/ac/cr/Devweb/repository/ReviewRepository.java
@@ -3,7 +3,11 @@ package ucr.ac.cr.Devweb.repository;
 import ucr.ac.cr.Devweb.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ucr.ac.cr.Devweb.model.User;
+
+import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    List<Review> findByFreelancerID(Long freelancerId);
 }
