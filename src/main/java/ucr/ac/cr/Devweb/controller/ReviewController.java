ec6dd4ebd2777ca3ef4f998969531d7fef1adca8
joshuaEA12 <joshuabenjasolanojimenes@gmail.com>
2026-05-21 19:36:55 -0600

    @DeleteMapping

--- src/main/java/ucr/ac/cr/Devweb/controller/ReviewController.java
+++ src/main/java/ucr/ac/cr/Devweb/controller/ReviewController.java
@@ -3,10 +3,7 @@ package ucr.ac.cr.Devweb.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ucr.ac.cr.Devweb.model.Review;
 import ucr.ac.cr.Devweb.service.ReviewService;
 
@@ -26,5 +23,12 @@ public class ReviewController {
             return ResponseEntity.ok("Freelancer " + id + " has no reviews");
         }
         return ResponseEntity.ok(reviews);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteReviewById(@PathVariable Long id){
+        this.reviewService.deleteReview(id);
+        return ResponseEntity.noContent().build();
     }
 }
