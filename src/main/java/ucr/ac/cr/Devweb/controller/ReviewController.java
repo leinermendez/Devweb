fab884bb2cfcec91290b300d44445079e2819e36
joshuaEA12 <joshuabenjasolanojimenes@gmail.com>
2026-05-21 19:55:55 -0600

    @PostMapping

--- src/main/java/ucr/ac/cr/Devweb/controller/ReviewController.java
+++ src/main/java/ucr/ac/cr/Devweb/controller/ReviewController.java
@@ -1,13 +1,18 @@
 package ucr.ac.cr.Devweb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ucr.ac.cr.Devweb.model.Review;
 import ucr.ac.cr.Devweb.service.ReviewService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/review")
@@ -25,6 +30,19 @@ public class ReviewController {
         return ResponseEntity.ok(reviews);
     }
 
+    @PostMapping
+    public ResponseEntity<?> saveReview(@Validated @RequestBody Review review, BindingResult result){
+        if (result.hasErrors()){
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError error : result.getFieldErrors()){
+                errors.put(error.getField(),error.getDefaultMessage());
+            }
+            return ResponseEntity.badRequest().body(errors);
+        }
+        Review newReview = this.reviewService.createReview(review);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(newReview);
+    }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteReviewById(@PathVariable Long id){
--- src/main/java/ucr/ac/cr/Devweb/service/ReviewService.java
+++ src/main/java/ucr/ac/cr/Devweb/service/ReviewService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import ucr.ac.cr.Devweb.model.Review;
 import ucr.ac.cr.Devweb.repository.ReviewRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -13,11 +14,19 @@ public class ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
 
+    //CREAR UNA NUEVA REVIEW OPTENIENDO LOS DATOS DEL FRONTEND
+    public Review createReview(Review review) {
+        review.setDate(LocalDateTime.now());
+        return this.reviewRepository.save(review);
+    }
+
+    //OPTENER LAS REVIEWS DEPENDIENDO DEL ID DEL FREELANCER
     public List<Review> findReviewFre(Long id){
         List<Review> resultd= this.reviewRepository.findByFreelancerID(id);
         return resultd;
     }
 
+    //ELIMINACION DE UNA REVIEW POR MEDIO DE SU ID
     public void deleteReview(Long id){
         this.deleteReview(id);
     }
