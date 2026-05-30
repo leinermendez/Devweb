9b8a4b23051561da6055cfeff8dda45a0a3b4914
BrandonViquezCamacho <lingtnig0xand@gmail.com>
2026-05-30 13:35:50 -0600

    Añadido repository de jobrequest

--- src/main/java/ucr/ac/cr/Devweb/repository/JobRequestRepository.java
+++ src/main/java/ucr/ac/cr/Devweb/repository/JobRequestRepository.java
@@ -4,8 +4,13 @@ import ucr.ac.cr.Devweb.model.JobRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface JobRequestRepository extends JpaRepository<JobRequest, Integer> {
+public interface JobRequestRepository extends JpaRepository<JobRequest, Long> {
+    List<JobRequest> findAllByClientId(Long clientId);
+
+    List<JobRequest> findAllByFreelancerId(Long freelancerId);
 }
 
 
