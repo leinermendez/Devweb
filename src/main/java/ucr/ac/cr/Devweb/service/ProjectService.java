4d1f9b6b03ed2cf68f4e52e05783b0c6b3f15ff2
Tocinit0 <leiner611@gmail.com>
2026-06-11 23:08:34 -0600

    "Actualizacion de metodo de crear"

--- src/main/java/ucr/ac/cr/Devweb/service/ProjectService.java
+++ src/main/java/ucr/ac/cr/Devweb/service/ProjectService.java
@@ -30,9 +30,9 @@ public class ProjectService {
     //CREAR UN NUEVO PROYECTO
     public ProjectDTO createProject(Project project) {
         Project saved = projectRepository.save(project);
-        return convertirProjectDTO(saved);
+        Project full = projectRepository.findById(saved.getId()).orElseThrow();
+        return convertirProjectDTO(full);
     }
-
     //OBETENER TODOS LOS PROYECTOS DE UN FREELANCER
     public List<ProjectDTO> getByFreelancerId(Long id){
         List<Project> projects = projectRepository.findByFreelancerId(id);
