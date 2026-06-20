520c87aa816f30bdd7c0d1465a0b3b3569e418e5
Tocinit0 <leiner611@gmail.com>
2026-06-19 19:43:58 -0600

    Actualizacion del feed

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,1 +1,19 @@
 const API_URL = "https://devweb-3lsj.onrender.com";
+
+async function cargarProyectos() {
+    const response = await fetch(API_URL + "/projects");
+    const proyectos = await response.json();
+    
+    const contenedor = document.getElementById("feedContainer");
+    contenedor.innerHTML = "";
+
+    proyectos.forEach(proyecto => {
+        contenedor.innerHTML += `
+            <div class="project-card">
+                <h3>${proyecto.title}</h3>
+                <p>Por: ${proyecto.freelancerName}</p>
+            </div>
+        `;
+    });
+}
+cargarProyectos();
