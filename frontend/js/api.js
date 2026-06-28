828cca365cae710d626fb61ff7e95128c562c8ac
BrandonViquezCamacho <lingtnig0xand@gmail.com>
2026-06-28 17:56:39 -0600

    Correción errores

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -3,7 +3,7 @@ const API_URL = "https://devweb-3lsj.onrender.com";
 function logout() {
     if (confirm("¿Seguro que querés cerrar sesión?")) {
         localStorage.removeItem("user");
-        window.location.href = "users/login.html";
+        window.location.href = "/users/login.html";
     }
 }
 
